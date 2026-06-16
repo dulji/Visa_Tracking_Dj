893e02dd3d4b01fa3d22f5edff5f2f2350176952
Dulaj Hewavithana <hewa.dulaj@gmail.com>
2026-06-16 08:30:38 +0530

    Initial commit

---
+++ src/main/java/com/example/visa_tracking_dj/VisaTrackingDjApplication.java
@@ -0,0 +1,13 @@
+package com.example.visa_tracking_dj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaTrackingDjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisaTrackingDjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visa_tracking_dj/VisaTrackingDjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visa_tracking_dj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaTrackingDjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
