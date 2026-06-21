09cd00422421dbb222fc06346e012f7d7213ba93
Dulaj Hewavithana <hewa.dulaj@gmail.com>
2026-06-21 20:50:31 +0530

    Finalizing AgencyRating and completeting Complaint

--- src/main/java/com/example/visa_tracking_dj/Dto/ComplaintDto.java
+++ src/main/java/com/example/visa_tracking_dj/Dto/ComplaintDto.java
@@ -1,4 +1,15 @@
 package com.example.visa_tracking_dj.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ComplaintDto {
+    private Integer complaintId;
+    private Integer agencyId;
+    private String description;
+    private Boolean status;
 }
