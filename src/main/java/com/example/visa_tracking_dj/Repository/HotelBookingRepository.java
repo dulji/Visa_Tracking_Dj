33e9545c456343be925af83d568d053f97c56a83
Dulaj Hewavithana <hewa.dulaj@gmail.com>
2026-06-27 17:47:02 +0530

    Changing structure to incorporate booking into hotelCheckIn as a separate strcture is not needed

--- src/main/java/com/example/visa_tracking_dj/Repository/HotelBookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.visa_tracking_dj.Repository;
-
-import com.example.visa_tracking_dj.Entity.HotelBookingMappingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelBookingRepository extends JpaRepository<HotelBookingMappingEntity, Integer> {
-
-}
