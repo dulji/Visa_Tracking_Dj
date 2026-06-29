f224f24cd14c2eb3d19225ffd51191ba75a6fe1f
Thisumitha <thisu2006@gmail.com>
2026-06-29 11:40:44 +0530

    email service test

--- src/main/java/com/example/visa_tracking_dj/Repository/AgencyTouristMappingRepository.java
+++ src/main/java/com/example/visa_tracking_dj/Repository/AgencyTouristMappingRepository.java
@@ -5,6 +5,7 @@ import com.example.visa_tracking_dj.Entity.AgencyTouristMappingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AgencyTouristMappingRepository extends JpaRepository<AgencyTouristMappingEntity, Integer> {
 
@@ -12,5 +13,5 @@ public interface AgencyTouristMappingRepository extends JpaRepository<AgencyTour
 
     boolean existsByTouristId(Long touristId);
     
-    java.util.Optional<AgencyTouristMappingEntity> findByTouristId(Long touristId);
+    Optional<AgencyTouristMappingEntity> findByTouristId(Long touristId);
 }
